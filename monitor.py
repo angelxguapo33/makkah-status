e225c87cf2c689a8a3cefcb2c6527b5c437ea4e9
angelxguapo33 <sisi.sou4884@gmail.com>
2026-05-10 13:41:23 +0100

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -7,7 +7,7 @@ import google.generativeai as genai
 # جلب المفتاح السري من إعدادات GitHub
 API_KEY = os.environ.get("GEMINI_API_KEY")
 # رابط البث المباشر لقناة القرآن الكريم
-YOUTUBE_URL = "https://www.youtube.com/watch?v=Fj0DqE_0d10"
+YOUTUBE_URL = "https://www.youtube.com/@SaudiQuranTv/live
 
 def save_status(status_text):
     with open("status.json", "w", encoding="utf-8") as f:
